f10bd32a64071c4b5a3293ee1f5129f8b03b484d
farrellhartomo <farrell.yodihartomo@gmail.com>
2018-02-22 16:13:47 +0700

    redefine object image

--- src/com/ccimg/itb/Plotting.java
+++ src/com/ccimg/itb/Plotting.java
@@ -1,7 +1,18 @@
 package com.ccimg.itb;
 
-
+import java.awt.image.*;
+import com.ccimg.itb.Image_Processor;
+import java.io.IOException;
 
 public class Plotting {
-
+//	init
+	Image_Processor img = new Image_Processor();
+	
+	
+	public Plotting() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	
 }
