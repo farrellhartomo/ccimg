eddd24f57d41baf5f7abe37aa772bb8f34fe58cd
farrellhartomo <farrell.yodihartomo@gmail.com>
2018-02-22 16:14:14 +0700

    redefine object processor image

--- src/com/ccimg/itb/Image_Processor.java
+++ src/com/ccimg/itb/Image_Processor.java
@@ -4,30 +4,50 @@ import javax.imageio.ImageIO;
 //import java.io.BufferedReader;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-//import java.io.File;
+import java.io.File;
 
 public class Image_Processor {
 	//init
+		BufferedImage image;
+		File file;
+		
 		int h1;
 		int w1;
 
 		int pixels[][];
-
-
+		String URI;
+		
+		
+		public Image_Processor() {
+			
+		}
+		
 		//reading image
-		public Image_Processor() throws IOException{
-			//File image_file = new File("/images/image3.jpg");
-			String URI = "/images/image3.jpg";
-			BufferedImage image = ImageIO.read(getClass().getResource(URI));
-			//image attributes
+		public Image_Processor(String uri) {
+			this.URI = uri;
+			try {
+				this.image = ImageIO.read(getClass().getResource(URI));
+			} catch (IOException e) { System.out.print(e);}
+		}
+		
+		public void setImage() throws IOException{
+			URI = "/images/image3.jpg";
+			this.image = ImageIO.read(getClass().getResource(URI));
 			h1 = image.getHeight();
 			w1 = image.getWidth();
 			pixels = new int[h1][w1];
 		}
 		
-
+		public void getImage(BufferedImage image) {
+//			if (image != null) {
+//				this.file = 
+//			}
+//			return file;
+		}
+		
 		public void getImageAtr(){
 			System.out.println("tinggi:"+h1);
 			System.out.print("lebar:" +w1);
 		}
+		
 }
--- src/com/ccimg/itb/Main.java
+++ src/com/ccimg/itb/Main.java
@@ -3,13 +3,10 @@ import com.ccimg.itb.Image_Processor;
 import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) {
-		try{
-			Image_Processor reader = new Image_Processor();
-			reader.getImageAtr();
-		} catch (IOException e){
-			System.out.print("error:" +e);
-		}
+	public static void main(String[] args) throws IOException {
+		Image_Processor reader = new Image_Processor();
+		reader.setImage();
+		reader.getImageAtr();
 	}
 
 }
